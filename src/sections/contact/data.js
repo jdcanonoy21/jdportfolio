088adb1ed18f2e01657f642a0b4509286ce01882
jdcanonoy <jdcanonoy21@gmail.com>
2023-03-02 23:22:54 +0800

    updated contact

--- src/sections/contact/data.js
+++ src/sections/contact/data.js
@@ -1,17 +1,11 @@
-import {HiOutlineMail} from 'react-icons/hi'
-import {RiMessengerLine} from 'react-icons/ri'
-import {FaWhatsapp} from 'react-icons/fa'
+import { HiOutlineMail } from "react-icons/hi";
+import { RiMessengerLine } from "react-icons/ri";
+import { FaWhatsapp } from "react-icons/fa";
 
 const data = [
-    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:dummyegator@gmail.com'},
-    {id: 2, icon: <RiMessengerLine/>, link: 'https://www.messenger.com/t/100087217880007'},
-    {id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/+233557097546'}
-]
+  { id: 1, icon: <HiOutlineMail />, link: "mailto:jdcanonoy21@gmail.com" },
+  { id: 2, icon: <RiMessengerLine />, link: "/" },
+  { id: 3, icon: <FaWhatsapp />, link: "/" },
+];
 
-
-// alternative whatsApp link
-// https://wa.me/yournumber
-// https://api.whatsapp.com/send/?phone=%2B233557097546
-
-
-export default data
+export default data;
