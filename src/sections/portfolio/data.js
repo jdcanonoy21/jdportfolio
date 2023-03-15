a296b23f7edbef0da8eb44b0baf96a0db8b5f45b
jdcanonoy <jdcanonoy21@gmail.com>
2023-03-15 15:22:10 +0800

    portfolio project updates

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -1,30 +1,33 @@
-import Image1 from "../../assets/etracs.jpg";
-import Image2 from "../../assets/ramesesinc.jpg";
-import Image3 from "../../assets/freelance-logos.jpg";
+import Image1 from "../../assets/portfolio.jpg";
+import Image2 from "../../assets/apollos.jpg";
 import Image4 from "../../assets/inkstone.jpg";
-import Image5 from "../../assets/portfolio.jpg";
+import Image3 from "../../assets/freelance-logos.jpg";
+import Image5 from "../../assets/ramesesinc.jpg";
+import Image6 from "../../assets/filipizen.jpg";
+import Image7 from "../../assets/etracs.jpg";
 
 const data = [
   {
     id: 1,
     category: "frontend",
     image: Image1,
-    title: "Etracs Website",
-    desc: "Etracs org emphasizes simplicity, balance, alignment, and contrast!",
-    demo: "https://etracs.org/index",
-    github: "https://github.com/jdcanonoy21/etracsorg.git",
-    alt: "etracs frontend design image",
+    title: "Portfolio",
+    desc: "My Portfolio",
+    demo: "https://sensational-kleicha-607ff1.netlify.app/",
+    github: "https://github.com/jdcanonoy21/jd-portfolio",
+    alt: "Portfolio Image",
   },
   {
     id: 2,
     category: "frontend",
     image: Image2,
-    title: "Rameses Inc Website",
-    desc: "Rameses Inc one page website",
-    demo: "https://www.ramesesinc.com/",
-    github: "",
-    alt: "Rameses Inc frontend design image",
+    title: "Apollos Travel",
+    desc: "A travel agency website",
+    demo: "https://64116f464d0d837d486f106b--leafy-quokka-43193c.netlify.app/",
+    github: "https://github.com/",
+    alt: "Apollos travel frontend design image",
   },
+
   {
     id: 3,
     category: "graphics",
@@ -49,11 +52,31 @@ const data = [
     id: 5,
     category: "frontend",
     image: Image5,
-    title: "Portfolio",
-    desc: "My Portfolio",
-    demo: "https://sensational-kleicha-607ff1.netlify.app/",
-    github: "https://github.com/jdcanonoy21/jd-portfolio",
+    title: "Rameses Inc Website",
+    desc: "Rameses Inc one page website",
+    demo: "https://www.ramesesinc.com/",
+    github: "",
+    alt: "Rameses Inc frontend design image",
+  },
+  {
+    id: 6,
+    category: "UX",
+    image: Image6,
+    title: "Filipizen UX Design",
+    desc: "Filipizen UX Design",
+    demo: "",
+    github: "",
     alt: "Portfolio Image",
+  },
+  {
+    id: 7,
+    category: "frontend",
+    image: Image7,
+    title: "Etracs Website",
+    desc: "Etracs org emphasizes simplicity, balance, alignment, and contrast!",
+    demo: "https://etracs.org/index",
+    github: "https://github.com/jdcanonoy21/etracsorg.git",
+    alt: "etracs frontend design image",
   },
 ];
 
