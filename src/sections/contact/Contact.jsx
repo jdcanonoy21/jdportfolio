d93eeed25194180f3f84a9212a8c563c2efe6948
jdcanonoy <jdcanonoy21@gmail.com>
2023-03-20 17:45:19 +0800

    contact edit

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -5,7 +5,7 @@ import "./Contact.scss";
 const Contact = () => {
   return (
     <section id="contact" className="contact">
-      <h2>Get In Touch</h2>\
+      <h2>Get in Touch</h2>
       <p>Shoot me a message via any of the links below!</p>
       <div className="container contact__container">
         {data.map((item) => (
