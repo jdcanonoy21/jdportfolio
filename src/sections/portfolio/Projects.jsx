ccd5cf39ec730f24d88396c94cd1d422c4d255e8
jdcanonoy <jdcanonoy21@gmail.com>
2023-03-29 20:12:51 +0800

    minor text correcttion

--- src/sections/portfolio/Projects.jsx
+++ src/sections/portfolio/Projects.jsx
@@ -1,14 +1,11 @@
 import Project from './Project';
-import Fade from 'react-reveal/Fade';
 
 const Projects = ({ projects }) => {
   return (
     <div className="container portfolio__projects">
-      <Fade bottom>
-        {projects.map((project) => (
-          <Project key={project.id} {...project} />
-        ))}
-      </Fade>
+      {projects.map((project) => (
+        <Project key={project.id} {...project} />
+      ))}
     </div>
   );
 };
