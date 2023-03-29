9f85d3b1d4502c0dfa9ae096db7bddaea420d31a
jdcanonoy <jdcanonoy21@gmail.com>
2023-03-29 20:11:05 +0800

    responisve mobile fix

--- src/sections/portfolio/Project.jsx
+++ src/sections/portfolio/Project.jsx
@@ -1,39 +1,36 @@
 import { Link } from 'react-router-dom';
 import './Portfolio.scss';
-import Fade from 'react-reveal/Fade';
 
 const Project = (props) => {
   const { title, image, desc, alt, demo, github, tools } = props;
   return (
     <div className="portfolio__project-content">
-      <Fade bottom>
-        <div className="portfolio__project-content-left">
-          <div className="portfolio__project-image">
-            <img src={image} alt={alt} />
-          </div>
+      <div className="portfolio__project-content-left">
+        <div className="portfolio__project-image">
+          <img src={image} alt={alt} />
         </div>
-        <div className="portfolio__project-content-right">
-          <h4>{title}</h4>
-          <p>{desc}</p>
-          <div className="portfolio__project-tools">
-            {tools.map((tool, i) => {
-              return <span key={i}>{tool}</span>;
-            })}
-          </div>
-          <div className="portfolio__project-cta">
-            {demo && (
-              <Link to={demo} className="btn sm">
-                Demo
-              </Link>
-            )}
-            {github && (
-              <Link to={github} className="btn sm">
-                Github
-              </Link>
-            )}
-          </div>
+      </div>
+      <div className="portfolio__project-content-right">
+        <h4>{title}</h4>
+        <p>{desc}</p>
+        <div className="portfolio__project-tools">
+          {tools.map((tool, i) => {
+            return <span key={i}>{tool}</span>;
+          })}
         </div>
-      </Fade>
+        <div className="portfolio__project-cta">
+          {demo && (
+            <Link to={demo} className="btn sm">
+              Demo
+            </Link>
+          )}
+          {github && (
+            <Link to={github} className="btn sm">
+              Github
+            </Link>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- src/sections/portfolio/Projects.jsx
+++ src/sections/portfolio/Projects.jsx
@@ -1,11 +1,14 @@
 import Project from './Project';
+import Fade from 'react-reveal/Fade';
 
 const Projects = ({ projects }) => {
   return (
     <div className="container portfolio__projects">
-      {projects.map((project) => (
-        <Project key={project.id} {...project} />
-      ))}
+      <Fade bottom>
+        {projects.map((project) => (
+          <Project key={project.id} {...project} />
+        ))}
+      </Fade>
     </div>
   );
 };
